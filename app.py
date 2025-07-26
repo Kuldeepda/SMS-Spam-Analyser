f46da68f8b6550ed4a43defc60da0e57dcffb121
Kuldeep  Das <137069029+Kuldeepda@users.noreply.github.com>
2025-07-26 10:59:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 
+
+# --- Setup for Render ---
+nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
+os.makedirs(nltk_data_dir, exist_ok=True)
+nltk.download('punkt', download_dir=nltk_data_dir)
+nltk.download('stopwords', download_dir=nltk_data_dir)
+nltk.data.path.append(nltk_data_dir)
+
+
 ps = PorterStemmer()
 
 
